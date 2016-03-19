b875115f3c45512f461a71cf0d44b7885135f813
lmedrzycki <lmedrzycki.jamiru.pl>
2016-03-19 10:47:02 +0100

    external mysql database connection

--- src/mtp/webservice/Constants.java
+++ src/mtp/webservice/Constants.java
@@ -2,15 +2,15 @@ package mtp.webservice;
 
 public class Constants {
 	public static String dbClass = "com.mysql.jdbc.Driver";
-	private static String dbName= "MTP";
-    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
-    public static String dbUser = "root";
-	public static String dbPwd = "1234";
+	private static String dbName= "db6004191_mtp";
+    public static String dbUrl = "jdbc:mysql://mysql636.cp.az.pl:3306/"+dbName;
+    public static String dbUser = "u6004191_mtp";
+	public static String dbPwd = "?w.<Z.1zk2GB*n(Q";
 	
 	public static String dbClassPg = "org.postgresql.Driver";
     public static String dbUserPg = "luke";
     public static String dbUrlPg = "jdbc:postgresql://localhost:5432/"+dbName;
     
-    public static String dbType = "POSTGRES";
+    public static String dbType = "MYSQL";
     
 }
