7b62c12eb6b9aca430e8b7cf5bb9fefdca7e9309
lmedrzycki <medrzycki.lukasz@gmail.com>
2016-09-26 09:39:55 +0200

    Heroku connection string

--- src/mtp/webservice/Constants.java
+++ src/mtp/webservice/Constants.java
@@ -3,9 +3,9 @@ package mtp.webservice;
 public class Constants {
 	public static String dbClass = "com.mysql.jdbc.Driver";
 	private static String dbName= "androidmtp";
-	public static String dbUrl = "jdbc:mysql://node33581-mtpandroid.unicloud.pl/"+dbName+"?useUnicode=yes&characterEncoding=UTF-8";
-	public static String dbUser = "luke";
-    public static String dbPwd = "123123";
+	public static String dbUrl = "jdbc:mysql://b9ca0e4a6a1bd9:f70feb74@eu-cdbr-west-01.cleardb.com/heroku_8f609e29676dcdb?reconnect=true?useUnicode=yes&characterEncoding=UTF-8";
+	public static String dbUser = "b9ca0e4a6a1bd9";
+    public static String dbPwd = "f70feb74";
 	
 	//private static String dbName= "db6004191_mtp";
     //public static String dbUrl = "jdbc:mysql://mysql636.cp.az.pl/"+dbName+"?characterEncoding=UTF-8";
